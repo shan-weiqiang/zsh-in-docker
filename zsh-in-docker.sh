6592f60c525bc36469dd375b0c0129f6c563d126
Shan Weiqiang <43443481+shan-weiqiang@users.noreply.github.com>
2023-12-29 15:55:42 +0800

    Update zsh-in-docker.sh

--- zsh-in-docker.sh
+++ zsh-in-docker.sh
@@ -114,9 +114,6 @@ zshrc_template() {
     fi
 
     cat <<EOM
-export LANG='en_US.UTF-8'
-export LANGUAGE='en_US:en'
-export LC_ALL='en_US.UTF-8'
 export TERM=xterm
 
 ##### Zsh/Oh-my-Zsh Configuration
